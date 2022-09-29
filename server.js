4de29966bbe4ebe8faed129383cc23c277309db8
Travis Geers <armyman1992@hotmail.com>
2022-09-28 23:47:22 -0600

    final touches on routing edits

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
+// importing dependencies 
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// Activates express and dedicates the port to use
+// initialize express and create port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware
+// middle ware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-// Error catch for invalid responses
+// default response for invalid requests
 app.use((req, res) => {
     res.status(404).end();
 });
 
-// Starts the server on the listed Port
+// start server and listen on PORT
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
 });

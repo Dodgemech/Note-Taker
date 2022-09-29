a03e78612217a4abd7ffdf4a287a3baf9143af2f
Travis Geers <armyman1992@hotmail.com>
2022-09-28 22:31:37 -0600

    changed route file names to index, continuing routes and added store.js to db

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', ApiRoutes);
+app.use('/', HtmlRoutes);
 
 // Error catch for invalid responses
 app.use((req, res) => {

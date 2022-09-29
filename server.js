6470d5a98a87091a82096ad0e4df3d873580659d
Travis Geers <armyman1992@hotmail.com>
2022-09-28 19:51:07 -0600

    requires express, made routes and andded middleware with error catch and list function to start the server.

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const apiRoutes = require('./routes/ApiRoutes');
+const htmlRoutes = require('./routes/HtmlRoutes');
+
+// Activates express and dedicates the port to use
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Error catch for invalid responses
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Starts the server on the listed Port
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`);
+});
